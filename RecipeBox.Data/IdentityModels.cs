defc2016e1f38fcec4b225f5c64ecca3dbf99593
Robert Hammer <bobbyhammer58@outlook.com>
2021-02-25 18:09:07 -0500

    users and source tables

--- RecipeBox.Data/IdentityModels.cs
+++ RecipeBox.Data/IdentityModels.cs
@@ -14,11 +14,13 @@ namespace RecipeBox.Data
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+        // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+        var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
             // Add custom user claims here
             return userIdentity;
         }
+
+        public string FullName { get; set; }//...................................................................................
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
