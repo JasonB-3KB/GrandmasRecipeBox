2f78ddfe964c50d1caf8952db35d90e8799b1c23
Jason Bagwill <mr.bagwill@gmail.com>
2021-03-06 14:53:51 -0500

    ownerid fix

--- RecipeBox.Services/CommentService.cs
+++ RecipeBox.Services/CommentService.cs
@@ -84,7 +84,7 @@ namespace RecipeBox.Services
                 var entity =
                     ctx
                     .Comments
-                    .Single(e => e.CommentId == commentId && e.OwnerId == _userId);
+                    .Single(e => e.CommentId == commentId);
                 return
                     new CommentDetail
                     {
@@ -102,7 +102,7 @@ namespace RecipeBox.Services
                 var entity =
                     ctx
                     .Comments
-                    .Single(e => e.CommentId == model.CommentId && e.OwnerId == _userId);
+                    .Single(e => e.CommentId == model.CommentId);
 
                 entity.CommentId = model.CommentId;
                 entity.Text = model.Text;
@@ -120,7 +120,7 @@ namespace RecipeBox.Services
                 var entity =
                     ctx
                     .Comments
-                    .Single(e => e.CommentId == commentId && e.OwnerId == _userId);
+                    .Single(e => e.CommentId == commentId);
 
                 ctx.Comments.Remove(entity);
 
